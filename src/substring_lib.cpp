af89d3c8a965c005f1e5697aa91c1c587aecc931
mediana105 <st087217@student.spbu.ru>
2025-02-11 14:22:48 +0300

    add suffix array to the library

--- src/substring_lib.cpp
+++ src/substring_lib.cpp
@@ -4,6 +4,7 @@
 
 #include "boyer-moore.h"
 #include "kmp.h"
+#include "suffix_array.h"
 #include "z_algorithm.h"
 
 std::vector<std::size_t> search_pattern(const std::string &text, const std::string &pattern, AlgorithmType type) {
@@ -14,6 +15,8 @@ std::vector<std::size_t> search_pattern(const std::string &text, const std::stri
             return BoyerMoore(pattern).search(text);
         case AlgorithmType::Z_ALGORITHM:
             return ZAlgorithm(pattern).search(text);
+        case AlgorithmType::SUFFIX_ARRAY:
+            return SuffixArray(pattern).search(text);
         default:
             throw std::invalid_argument("Unknown algorithm type");
     }
