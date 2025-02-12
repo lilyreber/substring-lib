e77bbedac4a087899e0c542424ea0ec07be9a34a
Arina Ivanova <arishkamu@gmail.com>
2025-02-12 22:27:46 +0300

    add benchmarks

--- experimental/benchmark.cpp
+++ experimental/benchmark.cpp
@@ -1,20 +1,4 @@
-#include <iostream>
+#include <benchmark/benchmark.h>
 
-#include "substring_lib.h"
-
-int main() {
-    auto kmp_res = search_pattern("aabaabcabbcabc", "abc", AlgorithmType::KMP);
-    std::cout << "kmp_res_size: " << kmp_res.size() << "\n";
-    std::cout << "kmp_res_0: " << kmp_res[0] << "\n"
-              << "kmp_res_1: " << kmp_res[1] << "\n";
-
-    auto bm_res = search_pattern("aabaabcabbcabc", "abc", AlgorithmType::BOYER_MOORE);
-    std::cout << "bm_res_size: " << bm_res.size() << "\n";
-    std::cout << "bm_res_0: " << bm_res[0] << "\n"
-              << "bm_res_1: " << bm_res[1] << "\n";
-
-    auto z_res = search_pattern("aabaabcabbcabc", "abc", AlgorithmType::Z_ALGORITHM);
-    std::cout << "z_res_size: " << z_res.size() << "\n";
-    std::cout << "z_res_0: " << z_res[0] << "\n"
-              << "z_res_1: " << z_res[1] << "\n";
-}
+// Run all registered benchmarks
+BENCHMARK_MAIN();
