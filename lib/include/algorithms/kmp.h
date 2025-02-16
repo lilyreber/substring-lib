834aa46aa07e973352fd67cf2765c4e5cafcd7ba
Arina Ivanova <arishkamu@gmail.com>
2025-02-16 16:27:30 +0100

    add comments

--- lib/include/algorithms/kmp.h
+++ lib/include/algorithms/kmp.h
@@ -4,12 +4,15 @@
 #include <string>
 #include <vector>
 
+// The KMP algorithm searches for a pattern in a text using the
+// LPS array to skip unnecessary comparisons, achieving O(n + m) time complexity.
 class KMP {
    public:
     explicit KMP(const std::string &pattern);
     std::vector<std::size_t> search(const std::string &text) const;
 
    private:
+    // for each prefix compute the size of the longest prefix equal suffix
     void computeLongestPrefixSuffixArray();
 
     std::string pattern;
