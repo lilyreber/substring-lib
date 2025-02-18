1db52cc044a1a642b92963302f194c719f93b117
kate <ikaterina0909@gmail.com>
2025-02-19 01:32:58 +0300

    add NOT_SO_NAIVE to AlgorithmType enum

--- lib/include/substring_lib.h
+++ lib/include/substring_lib.h
@@ -4,18 +4,8 @@
 #include <string>
 #include <vector>
 
-<<<<<<< Updated upstream
-    // store algorithm types
-    enum class AlgorithmType {
-        BOYER_MOORE,
-        KMP,
-        RABIN_KARP,
-        SUFFIX_ARRAY,
-        Z_ALGORITHM
-    };
-=======
+// store algorithm types
 enum class AlgorithmType { KMP, BOYER_MOORE, Z_ALGORITHM, SUFFIX_ARRAY, RABIN_KARP, NOT_SO_NAIVE };
->>>>>>> Stashed changes
 
 // common, public for library function
 std::vector<std::size_t> search_pattern(const std::string &text, const std::string &pattern, AlgorithmType type);
